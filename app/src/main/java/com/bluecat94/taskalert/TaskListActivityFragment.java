df3d4de13a94ff9dfb0175567ccaa91fe66ecf87
Mimi Liu <liumiru94@gmail.com>
2018-09-10 17:55:45 -0700

    add basic UI for task list activity

---
+++ app/src/main/java/com/bluecat94/taskalert/TaskListActivityFragment.java
@@ -0,0 +1,22 @@
+package com.bluecat94.taskalert;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TaskListActivityFragment extends Fragment {
+
+    public TaskListActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_task_list, container, false);
+    }
+}
