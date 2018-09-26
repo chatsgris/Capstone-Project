58bf6995af455aed155bc5b77d0dd9e7ff76854f
chatsgris <liumiru94@gmail.com>
2018-09-25 22:07:23 -0700

    start maps lite mode

--- app/src/main/java/com/bluecat94/taskalert/ui/TaskDetailActivityFragment.java
+++ app/src/main/java/com/bluecat94/taskalert/ui/TaskDetailActivityFragment.java
@@ -10,7 +10,13 @@ import android.widget.TextView;
 
 import com.bluecat94.taskalert.R;
 import com.bluecat94.taskalert.data.TasksContract;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.Map;
 
@@ -20,11 +26,12 @@ import butterknife.ButterKnife;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class TaskDetailActivityFragment extends Fragment {
+public class TaskDetailActivityFragment extends Fragment implements OnMapReadyCallback {
     private String mTitle;
     private String mDescription;
     private long mLong;
     private long mLat;
+    private GoogleMap mGoogleMap;
 
     @BindView(R.id.detail_button_delete) Button mButton;
     @BindView(R.id.detail_description_name) TextView mDescriptionName;
@@ -49,6 +56,26 @@ public class TaskDetailActivityFragment extends Fragment {
         ButterKnife.bind(this, view);
         mTitleValue.setText(mTitle);
         mDescriptionValue.setText(mDescription);
+
+
+            // Initialise the MapView
+            mMap.onCreate(null);
+            // Set the map ready callback to receive the GoogleMap object
+            mMap.getMapAsync(this);
+
         return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+        setMapLocation();
+    }
+
+    private void setMapLocation() {
+        if (mGoogleMap == null) return;
+        // Add a marker for this item and set the camera
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng((double) mLat, (double) mLong)));
     }
 }
